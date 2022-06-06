e54f79a3ecd746570b27e02ba50e64166c87b3de
Marcos Vinicius <marcosv9@hotmail.com>
2022-06-06 20:21:09 -0300

    new features
    linear regression implemented to plot pillar differences

--- obs_data_processing.py
+++ obs_data_processing.py
@@ -6,12 +6,24 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+from sklearn.linear_model import LinearRegression
 
 
-def p_diff(obs, starttime, endtime, path_gsm, pillar:int = 1):
+def p_diff(obs,
+           starttime,
+           endtime,
+           path_gsm,
+           pillar:int = 1
+           ):
     
-        
-    date_period = pd.date_range(starttime,endtime,freq = 'D')
+    
+    #list of days to check the existence of files    
+    date_period = pd.date_range(starttime,
+                                endtime,
+                                freq = 'D'
+                                )
+    
+    
     days_with_files = []
     real_days = []
     files_ppm = []
@@ -24,35 +36,46 @@ def p_diff(obs, starttime, endtime, path_gsm, pillar:int = 1):
     files_with_problems = []
     
     
+    # check the existence of previous pillars differences
+    # if not exists a new one will be created.
     try:
         df_pillar = pd.read_csv(f'pillar_differences_{obs}_p{pillar}.txt', sep = '\s+')
     except:
         df_pillar = pd.DataFrame(columns= [f'Date',
                                            f'p{pillar}_mean',
                                            f'p{pillar}_median',
-                                           f'p{pillar}_std'
+                                           f'p{pillar}_std',
+                                           f'jd'
                                            ]
                                  )
         df_pillar.to_csv(f'pillar_differences_{obs}_p{pillar}.txt', sep = ' ')
   
+   #cheking gsm files existence in the date period 
     for date in date_period:
         
         files_gsm.extend(glob.glob(f'{path_gsm}/{(datetime.strptime(str(date.date()), """%Y-%m-%d""").strftime(""""%Y%m%d"""))[3::]}*'))
         files_gsm.sort()
-        
+    
+   #creating list of dates only with gsm files existence.     
     for i in range(len(files_gsm)):
         
-        f = datetime.strptime(os.path.basename(files_gsm[i])[0:6], '%y%m%d').strftime('%Y%m%d')
-        days_with_files.append(f)
-
+        gsm_date = datetime.strptime(os.path.basename(files_gsm[i])[0:6], '%y%m%d').strftime('%Y%m%d')
+        days_with_files.append(gsm_date)
+    
+    
+    # cheking for ppm files in the days with gsm files.
     for i in days_with_files:
         files_ppm.extend(glob.glob(f'O:/jmat/{obs}/{obs}_{str(i)}.ppm'))
         files_ppm.sort()
-       
+    
+    #creating list of dates only with ppm files existence.    
     for i in range(len(files_ppm)):
         
-        f2 = os.path.basename(files_ppm[i])[5:13]
-        real_days.append(f2)
+        ppm_date = os.path.basename(files_ppm[i])[5:13]
+        real_days.append(ppm_date)
+        
+    #cheking if the days with gsm files are the same as the days with ppm files
+    #if they are different then, ppm days will be considered.
     
     if len(real_days) != len(days_with_files):
         
@@ -95,6 +118,8 @@ def p_diff(obs, starttime, endtime, path_gsm, pillar:int = 1):
             df_ppm.loc[df_ppm['F'] >= 99999.0, 'F'] = np.nan
 
             
+            #calculating mean, median and standard deviation between the gsm and ppm files
+            
             print(f'***********************************')
             print(f'Calculating statistics for {Date}')
             mean = round((df_gsm['F'] - df_ppm['F'].loc[str(df_gsm.index[0]): str(df_gsm.index[-1])]).dropna().mean(), 2)
@@ -108,12 +133,13 @@ def p_diff(obs, starttime, endtime, path_gsm, pillar:int = 1):
             print(f'\n The std was {stds} nT \n')
             #day = pd.to_datetime(Date,format= '%Y-%m-%d')
             days_index.append(Date)
-            
+            jd = jd2000(Date)
             
             df_tpm[f'Date'] = [Date]
             df_tpm[f'p{pillar}_mean'] = [mean]
             df_tpm[f'p{pillar}_median'] = [median]
             df_tpm[f'p{pillar}_std'] = [stds]
+            df_tpm[f'jd'] = [jd]
                 
             df_pillar = pd.read_csv(f'pillar_differences_{obs}_p{pillar}.txt', sep = '\s+')
 
@@ -128,14 +154,6 @@ def p_diff(obs, starttime, endtime, path_gsm, pillar:int = 1):
                 df_pillar = df_pillar[~df_pillar.index.duplicated(keep='last')]
                 df_pillar.sort_index().to_csv(f'pillar_differences_{obs}_p{pillar}.txt', sep = ' ')
             
-            
-            
-            #plt.figure(figsize = (12, 4))
-            #plt.plot(df_pillar['pillar_1_mean'], 'o', color = 'blue')
-            #plt.plot(df_tpm['pillar_1_mean'], 'o', color = 'red', label = 'New measurements')
-            #plt.legend()
-            #plt.show()
-                     
             
         except:
             
@@ -161,22 +179,29 @@ def p_diff(obs, starttime, endtime, path_gsm, pillar:int = 1):
     plt.savefig(f'pillar_differences_{obs}_p{pillar}.png', dpi = 300)
     plt.show()
     
-    print(f'****************************************************************'
-          f'\nList of files with problems - GSM files\n')
-    print(*files_with_problems, sep = '\n')   
+    if len(files_with_problems) > 0:
+        
+        print(f'****************************************************************'
+              f'\nList of files with problems - GSM files\n')
+        print(*files_with_problems, sep = '\n')   
     
     return days_index,means,medians,std
 
-def plot_pdiff(obs, pillar,  starttime = None, endtime = None):
+def plot_pdiff(obs,
+               pillar,
+               starttime = None,
+               endtime = None,
+               lr:bool = False
+               ):
     
-    path = f'pillar_differences_{obs}_p{pillar}.txt'
+    path = f'Geoomagnetic_observatory_data_processing/pillar_differences_{obs}_p{pillar}.txt'
          
     df_obs = pd.read_csv(path, sep = '\s+', index_col= [0])
 
     df_obs.index = pd.to_datetime(df_obs.index, format = '%Y-%m-%d')
     
     
-    if starttime == None and endtime == None:
+    if starttime == None and endtime == None and lr == False:
     
         fig, ax = plt.subplots(figsize=(18,5)) 
     #gs = gridspec.GridSpec(2, 1, height_ratios=[3, 1]) 
@@ -199,26 +224,53 @@ def plot_pdiff(obs, pillar,  starttime = None, endtime = None):
         ax.legend(loc='best', fontsize = 12)
         plt.show()
         
+        print('****************************************************************')
+        print('Pillar differences statistics for the period')          
         print('The maximum difference is:', round(df_obs[f'p{pillar}_mean'].max(),3))
         print('The minimum difference is:', round(df_obs[f'p{pillar}_mean'].min(),3))
         print('The median:', round(df_obs[f'p{pillar}_mean'].median(),3))
         print('The mean is:', round(df_obs[f'p{pillar}_mean'].mean(),3))
         print('The STD is:', round(df_obs[f'p{pillar}_mean'].std(),3))
+    
+    
+    if starttime == None and endtime == None and lr == True:
         
-    else:
+        prediction, model = linear_regression(df_obs['jd'], df_obs[f'p{pillar}_mean'])
+        
         fig, ax = plt.subplots(figsize=(18,5)) 
-    #gs = gridspec.GridSpec(2, 1, height_ratios=[3, 1]) 
-    #Days = ['01/01','03/01','05/01','07/01','09/01','11/01']
-    #ax0 = plt.subplot(gs[0])
+        ax.plot(df_obs['jd'],
+                df_obs[f'p{pillar}_mean'],
+                'or',
+                markersize = 5,
+                label = f'p{pillar}_mean')
+        ax.plot(df_obs['jd'],
+                prediction,
+                label = f'linear regression')
+
+        ax.set_ylabel('Differences(nT)', fontsize = 14)
+        ax.set_xlabel('Time', fontsize = 14)
+        ax.set_ylim(1.05*df_obs[f'p{pillar}_mean'].min(), 0.95*df_obs[f'p{pillar}_mean'].max())
+        ax.set_title(f'Pillar differences {obs.upper()}', fontsize = 16)
+        ax.grid()
+        ax.legend(loc='best', fontsize = 12)
+        plt.show()
+        print('****************************************************************')
+        print('Pillar differences statistics for the period')
+        print('The maximum difference is:', round(df_obs[f'p{pillar}_mean'].max(),3))
+        print('The minimum difference is:', round(df_obs[f'p{pillar}_mean'].min(),3))
+        print('The median:', round(df_obs[f'p{pillar}_mean'].median(),3))
+        print('The mean is:', round(df_obs[f'p{pillar}_mean'].mean(),3))
+        print('The STD is:', round(df_obs[f'p{pillar}_mean'].std(),3))      
+        print('****************************************************************')
+        print('Linear Regression')
+        print(print(f'y = {round((model.coef_),6)}x {round((model.intercept_),3)}'))
+      
+      
+      
+    if starttime != None and endtime != None and lr == False:  
+      
+        fig, ax = plt.subplots(figsize=(18,5)) 
         ax.plot(df_obs[f'p{pillar}_mean'][starttime:endtime], 'or',markersize = 5, label = f'p{pillar}_mean')
-#ax.plot(df1['jd'],pl(df1['jd']),'k--',label = 'linear trend = 0.002429x - 50.53', markersize = 4)
-#ax[0].plot(df2['doy'], df2['over'] , 'o--',label = 'GSM Over',markersize = 5)
-#ax[0].plot(DOY,pl(DOY),'r-',label = 'trend')
-#ax[0].set_xticklabels(Days)
-#ax.set_ylim(-35,-28)
-#ax[0].set_xticks(np.arange(0,365,61))
-#ax[0].axhline(y=np.mean(Diff), c='k', linewidth=2, alpha=0.5, linestyle='--',label = 'mean =-32,21 nT')
-#ax.set_xlim(7252,7669)
         ax.set_ylabel('Differences(nT)', fontsize = 14)
         ax.set_ylim(1.05*df_obs[f'p{pillar}_mean'][starttime:endtime].min(),0.95*df_obs[f'p{pillar}_mean'][starttime:endtime].max())
         ax.set_xlabel('Time', fontsize = 14)
@@ -227,9 +279,73 @@ def plot_pdiff(obs, pillar,  starttime = None, endtime = None):
         ax.legend(loc='best', fontsize = 12)
         plt.show()
         
+        print('****************************************************************')
+        print('Pillar differences statistics for the period')          
         print('The maximum difference is:', round(df_obs[f'p{pillar}_mean'][starttime:endtime].max(),3))
         print('The minimum difference is:', round(df_obs[f'p{pillar}_mean'][starttime:endtime].min(),3))
         print('The median:', round(df_obs[f'p{pillar}_mean'][starttime:endtime].median(),3))
         print('The mean is:', round(df_obs[f'p{pillar}_mean'][starttime:endtime].mean(),3))
         print('The STD is:', round(df_obs[f'p{pillar}_mean'][starttime:endtime].std(),3))
+        
+    if starttime != None and endtime != None and lr == True:
+        
+        prediction, model = linear_regression(df_obs[starttime:endtime]['jd'], df_obs[starttime:endtime][f'p{pillar}_mean'])
+        
+        fig, ax = plt.subplots(figsize=(18,5)) 
 
+        ax.plot(df_obs[starttime:endtime]['jd'],
+                df_obs[f'p{pillar}_mean'][starttime:endtime],
+                'or',
+                markersize = 5,
+                label = f'p{pillar}_mean')
+        
+        ax.plot(df_obs[starttime:endtime]['jd'],
+                prediction,
+                label = f'Linear Regression')
+        
+        ax.set_ylabel('Differences(nT)', fontsize = 14)
+        ax.set_ylim(1.05*df_obs[f'p{pillar}_mean'][starttime:endtime].min(),
+                    0.95*df_obs[f'p{pillar}_mean'][starttime:endtime].max()
+                    )
+        ax.set_xlabel('Time', fontsize = 14)
+        ax.set_title(f'Pillar differences {obs.upper()}', fontsize = 16)
+        ax.grid()
+        ax.legend(loc='best', fontsize = 12)
+        plt.show()
+        
+        print('****************************************************************')
+        print('Pillar differences statistics for the period')        
+        print('The maximum difference is:', round(df_obs[f'p{pillar}_mean'][starttime:endtime].max(),3))
+        print('The minimum difference is:', round(df_obs[f'p{pillar}_mean'][starttime:endtime].min(),3))
+        print('The median:', round(df_obs[f'p{pillar}_mean'][starttime:endtime].median(),3))
+        print('The mean is:', round(df_obs[f'p{pillar}_mean'][starttime:endtime].mean(),3))
+        print('The STD is:', round(df_obs[f'p{pillar}_mean'][starttime:endtime].std(),3))        
+        print('****************************************************************')
+        print('Linear Regression')
+        print(print(f'y = {round(float(model.coef_),6)}x {round(float(model.intercept_),3)}'))        
+           
+        
+    return df_obs
+
+
+def jd2000(date):
+    
+    date = datetime.strptime(date, '%Y-%m-%d')
+    return int((date - datetime(2000,1,1)).days)
+
+def linear_regression(x, y):
+    
+    y = y.dropna()
+    x = x.dropna()
+    
+    x = x.reindex(y.index)
+    
+    x_train = x.values.reshape(-1,1)
+    y_train = y 
+    
+    model = LinearRegression()
+    model.fit(x_train, y_train)
+    
+    prediction = model.predict(x_train)
+    
+    return prediction, model
